92125403eb43459d5f58b7b11a4514c234897745
Oleh Dokuka <shadowgun@i.ua>
2017-02-20 11:47:29 +0200

    provisionig should prolomg even if failure presents in log

--- src/main/flow/strategy/DeploymentInfoStrategy.ts
+++ src/main/flow/strategy/DeploymentInfoStrategy.ts
@@ -6,10 +6,10 @@ interface ExecutionError extends Error {
 
 export class DeploymentInfoStrategy extends SSHStrategy<void, DeployInfo, ExecutionError> {
     public wrap(args: void): Promise<DeployInfo | ExecutionError> {
-        return this.client.execCommand('cat /opt/install/package/deploy.log | grep \'Finished!\'')
+        return this.client.execCommand('cat /opt/install/package/deploy.log | grep -E -i -w \'Finished|There was a problem while starting tomcat.|Could not start the tomcat correctly.\'')
             .then<DeployInfo | ExecutionError>(r =>
                 r.stderr
                     ? Promise.reject(new Error(r.stderr))
-                    : Promise.resolve({ isFinished: r.stdout === 'Finished!' }))
+                    : Promise.resolve({ isFinished: r.stdout.length > 0 }))
     }
 }
